83454b436ce5a78259480806d3014210dc4cb0e9
govind kumar <gk4051668@gmail.com>
2022-11-14 20:25:19 +0530

    completed all tasks

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,41 +1,47 @@
-import { useState } from 'react';
-const Login = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const Login = ({ setUser }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!name || !email) return;
+    setUser({ name, email });
+    navigate("/dashboard");
   };
 
   return (
-    <section className='section'>
-      <form className='form' onSubmit={handleSubmit}>
+    <section className="section">
+      <form className="form" onSubmit={handleSubmit}>
         <h5>login</h5>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
           <input
-            type='text'
-            className='form-input'
-            id='name'
+            type="text"
+            className="form-input"
+            id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             email
           </label>
           <input
-            type='email'
-            className='form-input'
-            id='email'
+            type="email"
+            className="form-input"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           login
         </button>
       </form>
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import products from "../data.js";
 
 const SingleProduct = () => {
   const { productId } = useParams();
-  console.log(productId);
+  const product = products.find((product) => product.id === productId);
+  const { image, name } = product;
   return (
     <section className="section product">
-      <h2>single product</h2>
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
+      <Link to="/products">back to products</Link>
     </section>
   );
 };
