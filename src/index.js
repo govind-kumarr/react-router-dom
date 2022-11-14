755395b39a86045e8a3bbde5bfe4f14bb0b38825
govind kumar <gk4051668@gmail.com>
2022-11-14 20:26:57 +0530

    completed all tasks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import App from './final/App';
+// import App from './final/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
       <App />
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
